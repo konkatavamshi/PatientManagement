9a9c2562939378a9313af25c9f430bffda522cc8
harshareddy-thotli <harshareddy2038@gmail.com>
2026-09-10 13:41:57 +0530

    Add Builder annotation to PatientAddressResponseDTO

--- src/main/java/com/flm/patient/dto/PatientAddressResponseDTO.java
+++ src/main/java/com/flm/patient/dto/PatientAddressResponseDTO.java
@@ -2,11 +2,13 @@ package com.flm.patient.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class PatientAddressResponseDTO {
 
 	private long patientAddressId;
